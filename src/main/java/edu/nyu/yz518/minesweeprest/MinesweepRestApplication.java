44162851ca0ffb5aa5f107e752e640b4d15a5aa1
nyuyz518 <lilyzhangyun@gmail.com>
2021-05-06 21:29:36 -0400

    initial spring-boot

---
+++ src/main/java/edu/nyu/yz518/minesweeprest/MinesweepRestApplication.java
@@ -0,0 +1,13 @@
+package edu.nyu.yz518.minesweeprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweepRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweepRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nyu/yz518/minesweeprest/MinesweepRestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nyu.yz518.minesweeprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweepRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
